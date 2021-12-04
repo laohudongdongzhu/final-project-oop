2d54a964422c98caf28f1ef705095cf8a3adb633
laohudongdongzhu <90426498+laohudongdongzhu@users.noreply.github.com>
2021-12-03 23:33:14 -0500

    Add files via upload

---
+++ StockClass(1).h
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+#include <vector>
+
+class StockClass
+{
+private:
+    std::vector<float> data;
+
+public:
+    void setData(float);
+    float calcDailyPnL(int);
+    float calcMovingAvg(int, int);
+    float getDayData(int);
+};
---
+++ main(1).cpp
@@ -0,0 +1,26 @@
+﻿// stock_backtest.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <fstream>
+#include "StockClass.h"
+using namespace std;
+
+int main()
+{
+	string fileName = "TSLA.txt";
+	ifstream file;
+	float input;
+	file.open(fileName);
+	StockClass tsla;
+
+	for (int i = 0; i < 2881; i++) {
+		file >> input;
+		tsla.setData(input);
+		cout << tsla.getDayData(i) << endl;
+	}
+
+}
